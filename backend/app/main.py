eb7b5699556629da3af1e23cb99d15c3ac709b2e
Aditya <adityavegulla@gmail.com>
2026-06-06 11:57:13 +0530

    Refine CORS middleware configuration to specify allowed origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,8 +11,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    all_origins=["*"],
-    all_credentials=True,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
